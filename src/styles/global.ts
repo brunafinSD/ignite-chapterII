bc940d628711b47c4802accfec3bb59cee081c17
Bruna Nunes <bruna.f.n@hotmail.com>
2021-07-14 10:16:11 -0300

    fontes do google fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
     :root{
-        
+
         --background: #f0f2f5;
         --shape: #ffffff;
 
@@ -44,6 +44,15 @@ export const GlobalStyle = createGlobalStyle`
     body{
         background: var(---background);
         -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button{
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong{
+        font-weight: 600;
     }
 
     button{
