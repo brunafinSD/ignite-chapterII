5eba91dee5228e6376a2ad849e79861d0a7f145a
Bruna Nunes <bruna.f.n@hotmail.com>
2021-07-15 11:00:15 -0300

    configurando modal de criação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,26 @@
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+    // quando recebemos nas props uma função começa com "on"
+    // () => void significa que a função não recebe nenhum parâmetro e não retorna nada
+    onOpenNewTransaction: () => void;
+}
+
+export function Header({onOpenNewTransaction} : HeaderProps) {
     return (
         <Container>
             <Content>
+                
             <img src={logoImg} alt="dt money" />
-            <button type="button">
+
+            <button
+            type="button"
+            onClick={onOpenNewTransaction}
+            >
                 Nova transação
             </button>
+
             </Content>
         </Container>
     );
