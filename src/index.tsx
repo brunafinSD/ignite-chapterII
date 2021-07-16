7fa2c54a3f1e86f08052424acb1454341169ff95
Bruna Nunes <bruna.f.n@hotmail.com>
2021-07-16 11:57:50 -0300

    listando transações e seeds

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,30 @@ createServer({
   models:{
     transaction: Model,
   },
+
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Almoço',
+          type: 'withdraw',
+          category: 'compras',
+          amount: 25,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Venda de pulseiras',
+          type: 'deposit',
+          category: 'freelance',
+          amount: 15,
+          createdAt: new Date('2021-02-15 15:00:00'),
+        }
+      ]
+    })
+  },
+
   routes(){
     this.namespace = 'api';
     this.get('/transactions', () => {
