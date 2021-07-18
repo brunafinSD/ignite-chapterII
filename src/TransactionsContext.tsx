7878ef86c9f39a31dadf65cf52a78fdeeb9a2805
Bruna Nunes <bruna.f.n@hotmail.com>
2021-07-18 19:58:41 -0300

    finalizando inserção

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -21,7 +21,7 @@ interface TransactionsProviderProps {
 
 interface TransactionsContextData {
     transactions: Transaction[];
-    createTransaction: (transaction: TransactionInput) => void;
+    createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
 export const TransactionsContext = createContext<TransactionsContextData>(
@@ -36,9 +36,15 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
             .then(response => setTransactions(response.data.transactions));
     }, []);
 
-    function createTransaction(transaction: TransactionInput) {
+    async function createTransaction(transactionInput: TransactionInput) {
 
-        api.post('/transactions', transaction);
+        const response = await api.post('/transactions', {
+            ...transactionInput,
+            createdAt: new Date(),
+        });
+        const { transaction } = response.data;
+
+        setTransactions([...transactions, transaction]);
     }
 
     return (
